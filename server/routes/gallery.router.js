a6a91a1a7ab77ffb48337298bbd4a4ce64fb2adc
Maggie Jenkins <adair.jenkins@gmail.com>
2022-03-27 07:56:44 -0500

    add database routes

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,50 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
-// DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const id = req.params.id;
+    console.log('PUT id:', id);
+    
+    const queryText = `
+        UPDATE "items"
+        SET "likes" = "likes" + 1
+        WHERE "id" = $1;
+    `;
+    const values = [id];
+    console.log("updated database likes");
+
+    pool.query(queryText, values)
+        .then( result => {
+            res.sendStatus(200);
+        })
+        .catch( err => {
+            console.log(err);
+            res. sendStatus(500);
+        })
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    console.log('GET route');
+
+    const queryText = `
+        SELECT * FROM "items"
+        ORDER BY "artist"
+    `;
+
+    pool.query(queryText)
+        .then( result => {
+            console.log(result.rows);
+            res.send(result.rows);
+        })
+        .catch( err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 }); // END GET Route
 
 module.exports = router;
